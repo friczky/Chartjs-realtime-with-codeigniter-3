f7933f42302b5c38593af1611a2068ba89ec2570
Fadilah Riczky <friczky@gmail.com>
2023-07-16 15:58:33 +0700

    fix error

--- application/models/AirModel.php
+++ application/models/AirModel.php
@@ -7,4 +7,10 @@ class AirModel extends CI_Model
         $query = $this->db->get('air');
         return $query->result_array();
     }
+
+    public function get_by_month($bulan)
+    {
+        $query = $this->db->query("SELECT * FROM view_air where bulan = $bulan");
+        return $query->result_array();
+    }
 }
